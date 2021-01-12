b92d6117eb1230c5cd60b7962c49e46f432b6977
Yaroslav <yaroslav8oroboro@gmail.com>
2021-01-12 14:19:54 +0200

    added hw-3 implementation

--- src/main/java/project/models/Manufacturer.java
+++ src/main/java/project/models/Manufacturer.java
@@ -20,18 +20,6 @@ public class Manufacturer {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
     public void setCountry(String country) {
         this.country = country;
     }
